87d18df0ee6f47785d87120c6fc5c88e37ece632
nmackee <mackee.base@gmail.com>
2020-11-13 02:32:40 +0900

    'resources' を使った「CRUDアプリ」に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to action: :index
+    #redirect_to action: :index
   end
 
   def edit
@@ -20,13 +20,13 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to action: :index
+    #redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to action: :index
+    #redirect_to action: :index
   end
 
   private
